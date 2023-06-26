e11e7b1072ae7b4e45b4e9c8e45ea093f90000b6
Simon Huang <huisimonliu@gmail.com>
2023-06-26 12:58:48 -0700

    Cursor Moves to Collect Reward and Close game

---
+++ CloseGame.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WarThunderDailyRewardConsoleApp
+{
+    // Using INPUT SIMULATOR PACKAGE
+    internal class CloseGame
+    {
+        // Close War thunder application
+        // Control Sequence close game
+        // downArrow -> esc > 2xdownArrow -> enter -> leftarrow -> enter
+        public CloseGame()
+        {
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+   UIntPtr dwExtraInfo);
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetActiveWindow(IntPtr hWnd);
+
+        public void RunCloseGameScript(IntPtr game) 
+        {
+            SetForegroundWindow(game);
+            SetActiveWindow(game);
+            double height = SystemParameters.FullPrimaryScreenHeight;
+            double width = SystemParameters.FullPrimaryScreenWidth;
+
+            Debug.WriteLine("Closing Game" );
+            Debug.WriteLine(height);
+            Debug.WriteLine(width);
+
+            while (true)
+            {
+                SetActiveWindow(game);
+                Thread.Sleep(5000);
+
+                // Close Update / Friend Referral Pop up
+                Cursor.Position = new System.Drawing.Point(1504,79); // Close Update X
+                Thread.Sleep(2000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+                Debug.WriteLine("ButtonPress");
+
+
+
+
+                // Collect Reward
+                Cursor.Position = new System.Drawing.Point(963, 484); // Collect Reward
+                Thread.Sleep(2000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+
+                Cursor.Position = new System.Drawing.Point(1349, 746); // Close Reward
+                Thread.Sleep(2000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+
+
+                // Open Close Game Menu
+                Cursor.Position = new System.Drawing.Point(88, 30); // 3 Dash Menu
+                Thread.Sleep(2000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+
+                Cursor.Position = new System.Drawing.Point(172, 117); // Move to Exit Game Button
+                Thread.Sleep(3000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+
+
+                // Exit game Pop up
+                Cursor.Position = new System.Drawing.Point(895, 570); // Move to Exit Game Button
+                Thread.Sleep(2000);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, new System.UIntPtr());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, new System.UIntPtr());
+
+
+                Debug.WriteLine(Cursor.Position);
+               
+            }
+
+            Debug.WriteLine("Finished Script");
+        }
+        private const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WarThunderDailyRewardConsoleApp
 {
     internal class Program
     {
+        static int SLEEPMS = 10000;
+
         static void Main(string[] args)
         {
+            CloseGame closeGame = new CloseGame();
+
             Process proc = new Process();
             // Start war thunder exec from steam
             proc.StartInfo.FileName = "D://Steam/steam.exe";
@@ -22,8 +28,9 @@ namespace WarThunderDailyRewardConsoleApp
             proc.Start();
 
             // Wait for 30 seconds
-            System.Threading.Thread.Sleep(20000);
+            System.Threading.Thread.Sleep(SLEEPMS);
 
+            IntPtr game = IntPtr.Zero;
             // Get War thunder process
             Process[] processCollection = Process.GetProcesses();
             foreach (Process p in processCollection)
@@ -31,15 +38,24 @@ namespace WarThunderDailyRewardConsoleApp
                 if ((string)p.ProcessName == "aces") 
                 {
                     Debug.WriteLine("War Thunder is open");
+                    game = p.MainWindowHandle;
+                    Debug.WriteLine(game);
                 }
                 //Debug.WriteLine(p.ProcessName);
             }
-
+            // Focus War thunder on computer
+            if (game != IntPtr.Zero) 
+            {
+                closeGame.RunCloseGameScript(game);
+            }
+            // Close Game
+            
             // Schedule Task run war thunder
-
             // Program presses enter twice after 30 seconds
             // steam://rungameid/236390
             // Close War thunder application
+            // Control Sequence close game
+            // downArrow -> esc > 2xdownArrow -> enter -> leftarrow -> enter
         }
     }
 }
